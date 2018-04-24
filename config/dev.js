06f3f688753826ba7aca4c7e0706261bb557b360
James Yu <james_yu@asus.com>
2018-04-24 11:15:26 +0800

    mLab

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://james_yu:asus#1234@ds255539.mlab.com:55539/blog_dev',
   cookieKey: '123123123'
 };
