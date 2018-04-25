c5d03c9e4c28d3f9b7fa5524d788026834c4088e
James Yu <james_yu@asus.com>
2018-04-25 11:02:19 +0800

    Toggleable Cache

--- services/cache.js
+++ services/cache.js
@@ -7,7 +7,16 @@ const client = redis.createClient(redisUrl)
 client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec
 
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true
+  return this // chainable
+}
+
 mongoose.Query.prototype.exec = async function () { // 需要this指向Query，並且能使用await
+  if (!this.useCache) {
+    return exec.apply(this, arguments)
+  }
+
   const key = JSON.stringify(Object.assign({}, this.getQuery(), { // safely copy object from one to another
     collection: this.mongooseCollection.name
   }))
