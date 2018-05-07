d846ab7cc40bb56e39eaf66c8cfa0ffaa3e3ec46
James Yu <james_yu@asus.com>
2018-05-07 15:02:41 +0800

    CI: Travis

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://james_yu:asus#1234@ds255539.mlab.com:55539/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -5,7 +5,8 @@ const userFactory = require('../factories/userFactory')
 class CustomPage {
   static async build () {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: true, //
+      args: ['--no-sandbox']
     })
 
     const page = await browser.newPage()
@@ -33,7 +34,7 @@ class CustomPage {
     await page.setCookie({name: 'session', value: session})
     await page.setCookie({name: 'session.sig', value: sig})
 
-    await page.goto('localhost:3000/blogs')
+    await page.goto('http://localhost:3000/blogs')
     await page.waitFor('a[href="/auth/logout"]')
   }
 
